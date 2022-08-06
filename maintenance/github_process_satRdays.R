824551607e5ef7aff92ac186e7e036418abcaa9d
Ehouman Evans <36210209+EhoumanEvans@users.noreply.github.com>
2022-08-06 10:50:30 +0000

    Update github_process_satRdays.R

--- maintenance/github_process_satRdays.R
+++ maintenance/github_process_satRdays.R
@@ -7,43 +7,37 @@ usethis::use_git_config(
   user.email = " ")
 
 
-# Install Git before doing anything
-# Tool/version control/Project Set up/Set version Control to GIT
+# Activer Git pour son projet
+# Tool/Project Option/GitSVN/Version control system
 
-# Method 1
-# This method is more complicated,
-# Connect a local folder to Github
+# Method 1 : Connecter son dossier local à Github
+# La méthode la plus compliquée
 
-# Configure Git after installation
+
+# Configurer GitHub en local
 library(usethis)
 edit_git_config()
 
-
-
-# Create a Personal Access Token (PAT) on GitHub
+# Créer un Personal Access Token (PAT) sur GitHub 
 library(usethis)
-
 create_github_token()
-  # Copy the token Generated in Github
-  # and save is in you .gitconfig
+# Copier le token Generated sur Github
+# and save is in you .gitconfig
+# ou aller dans son compte pour générer le token via : /Github/profile/Settings / Developer settings/Personal access token
 
 # Relaunch RStudio
 # Store Personal Access Token to Connect RStudio and GitHub
-
 library(gitcreds)
 gitcreds_set()
 
-
 # Relaunch RStudio
-# Connect RStudio Projects with GitHub Repositories
-# RStudio First
 library(usethis)
 use_github()
 
 
 # Mehtode 2
 # Create a folder in local in a project
-# Then create the same project on  line on github
+# Then create the same project on line on github
 # Copy the second bloc option
 # Have already you TOKEN copied
 # Paste the copied code in the Terminal of the current projet thot you could to connect online.
@@ -56,6 +50,8 @@ use_github()
 # Create a new version control project
 # Paste the link and create a local folder
 # Shape the structure of your project folder
+
+
 
 
 
